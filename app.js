6f1e77fe4e720cf6f12c7b194ea824accf40d327
song <song@mc2.io>
2014-05-19 21:15:24 +0800

    add layout for basic view

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var App = function() {
     that.dataReady = false;
     that.stackId = null;
 
-    $(document).on('click', '.run-stack', function(event) {
+    $(document).on('click', '#intro .run', function(event) {
         var btnDom = $(event.currentTarget);
         var stackId = btnDom.data('id');
         $.cookie('stack_store_id', stackId, {
@@ -16,8 +16,23 @@ var App = function() {
         });
     });
 
+    $(document).on('click', '#main .stack-item a', function(event) {
+        $('#main').removeClass('animation-show').addClass('animation-hidden');
+        $('#intro').removeClass('animation-hidden').addClass('animation-show');
+    });
+
+    $(document).on('click', '#intro .nav', function(event) {
+        $('#main').show();
+        $('#main').removeClass('animation-hidden').addClass('animation-show');
+        $('#intro').removeClass('animation-show').addClass('animation-hidden');
+    });
+
     routie(':stackId', function(stackId) {
         that.stackId = stackId;
+        if (stackId && stackId !== 'index') {
+            
+        }
+        $('#main').hide();
         if (that.dataReady) {
             that.renderStackIntro(stackId);
         }
@@ -51,13 +66,247 @@ App.prototype.getStateStoreData = function(callback) {
                         "id": "cassandra",
                         "name": "cassandra",
                         "description": "cassandra",
-                        "introduce": "cassandra"
+                        "introduce": "In this article, I’m going to examine the science behind making successful UI icons before teaching you how to make your own embeddable icon font. From designing the individual icons to converting them for @font-face embedding, and even licensing them for distribution, we shall be using only free software and online services. How about that? You will not need to rely on any of the esoteric knowledge required to make successful alphanumeric typefaces; just an eye for designing things that may appear very, very small. Ultimately, you should go away with a process for making design elements that extends far beyond the manufacture of simple icons. Before we continue, something should be said about what exactly we are trying to achieve by using icons in our designs in the first place, and what makes one icon more successful than the next. Theory before application. In order to do this, we must consider the icon’s role as part of semiology."
                     },
                     {
                         "id": "ghost-blog",
                         "name": "ghost-blog",
                         "description": "ghost-blog",
                         "introduce": "ghost-blog"
+                    },
+                    {
+                        "id": "mongo-cluster",
+                        "name": "mongo-cluster",
+                        "description": "mongo-cluster",
+                        "introduce": "mongo-cluster"
+                    },
+                    {
+                        "id": "nginx-upstream",
+                        "name": "nginx-upstream",
+                        "description": "nginx-upstream",
+                        "introduce": "nginx-upstream"
+                    },
+                    {
+                        "id": "redis-cluster",
+                        "name": "redis-cluster",
+                        "description": "redis-cluster",
+                        "introduce": "redis-cluster"
+                    },
+                    {
+                        "id": "apache-hadoop",
+                        "name": "apache-hadoop",
+                        "description": "apache-hadoop",
+                        "introduce": "apache-hadoop"
+                    },
+                    {
+                        "id": "cassandra",
+                        "name": "cassandra",
+                        "description": "cassandra",
+                        "introduce": "In this article, I’m going to examine the science behind making successful UI icons before teaching you how to make your own embeddable icon font. From designing the individual icons to converting them for @font-face embedding, and even licensing them for distribution, we shall be using only free software and online services. How about that? You will not need to rely on any of the esoteric knowledge required to make successful alphanumeric typefaces; just an eye for designing things that may appear very, very small. Ultimately, you should go away with a process for making design elements that extends far beyond the manufacture of simple icons. Before we continue, something should be said about what exactly we are trying to achieve by using icons in our designs in the first place, and what makes one icon more successful than the next. Theory before application. In order to do this, we must consider the icon’s role as part of semiology."
+                    },
+                    {
+                        "id": "ghost-blog",
+                        "name": "ghost-blog",
+                        "description": "ghost-blog",
+                        "introduce": "ghost-blog"
+                    },
+                    {
+                        "id": "mongo-cluster",
+                        "name": "mongo-cluster",
+                        "description": "mongo-cluster",
+                        "introduce": "mongo-cluster"
+                    },
+                    {
+                        "id": "nginx-upstream",
+                        "name": "nginx-upstream",
+                        "description": "nginx-upstream",
+                        "introduce": "nginx-upstream"
+                    },
+                    {
+                        "id": "redis-cluster",
+                        "name": "redis-cluster",
+                        "description": "redis-cluster",
+                        "introduce": "redis-cluster"
+                    },
+                    {
+                        "id": "mongo-cluster",
+                        "name": "mongo-cluster",
+                        "description": "mongo-cluster",
+                        "introduce": "mongo-cluster"
+                    },
+                    {
+                        "id": "nginx-upstream",
+                        "name": "nginx-upstream",
+                        "description": "nginx-upstream",
+                        "introduce": "nginx-upstream"
+                    },
+                    {
+                        "id": "redis-cluster",
+                        "name": "redis-cluster",
+                        "description": "redis-cluster",
+                        "introduce": "redis-cluster"
+                    },
+                    {
+                        "id": "apache-hadoop",
+                        "name": "apache-hadoop",
+                        "description": "apache-hadoop",
+                        "introduce": "apache-hadoop"
+                    },
+                    {
+                        "id": "cassandra",
+                        "name": "cassandra",
+                        "description": "cassandra",
+                        "introduce": "In this article, I’m going to examine the science behind making successful UI icons before teaching you how to make your own embeddable icon font. From designing the individual icons to converting them for @font-face embedding, and even licensing them for distribution, we shall be using only free software and online services. How about that? You will not need to rely on any of the esoteric knowledge required to make successful alphanumeric typefaces; just an eye for designing things that may appear very, very small. Ultimately, you should go away with a process for making design elements that extends far beyond the manufacture of simple icons. Before we continue, something should be said about what exactly we are trying to achieve by using icons in our designs in the first place, and what makes one icon more successful than the next. Theory before application. In order to do this, we must consider the icon’s role as part of semiology."
+                    },
+                    {
+                        "id": "ghost-blog",
+                        "name": "ghost-blog",
+                        "description": "ghost-blog",
+                        "introduce": "ghost-blog"
+                    },
+                    {
+                        "id": "mongo-cluster",
+                        "name": "mongo-cluster",
+                        "description": "mongo-cluster",
+                        "introduce": "mongo-cluster"
+                    },
+                    {
+                        "id": "nginx-upstream",
+                        "name": "nginx-upstream",
+                        "description": "nginx-upstream",
+                        "introduce": "nginx-upstream"
+                    },
+                    {
+                        "id": "redis-cluster",
+                        "name": "redis-cluster",
+                        "description": "redis-cluster",
+                        "introduce": "redis-cluster"
+                    },
+                    {
+                        "id": "apache-hadoop",
+                        "name": "apache-hadoop",
+                        "description": "apache-hadoop",
+                        "introduce": "apache-hadoop"
+                    },
+                    {
+                        "id": "cassandra",
+                        "name": "cassandra",
+                        "description": "cassandra",
+                        "introduce": "In this article, I’m going to examine the science behind making successful UI icons before teaching you how to make your own embeddable icon font. From designing the individual icons to converting them for @font-face embedding, and even licensing them for distribution, we shall be using only free software and online services. How about that? You will not need to rely on any of the esoteric knowledge required to make successful alphanumeric typefaces; just an eye for designing things that may appear very, very small. Ultimately, you should go away with a process for making design elements that extends far beyond the manufacture of simple icons. Before we continue, something should be said about what exactly we are trying to achieve by using icons in our designs in the first place, and what makes one icon more successful than the next. Theory before application. In order to do this, we must consider the icon’s role as part of semiology."
+                    },
+                    {
+                        "id": "ghost-blog",
+                        "name": "ghost-blog",
+                        "description": "ghost-blog",
+                        "introduce": "ghost-blog"
+                    },
+                    {
+                        "id": "mongo-cluster",
+                        "name": "mongo-cluster",
+                        "description": "mongo-cluster",
+                        "introduce": "mongo-cluster"
+                    },
+                    {
+                        "id": "nginx-upstream",
+                        "name": "nginx-upstream",
+                        "description": "nginx-upstream",
+                        "introduce": "nginx-upstream"
+                    },
+                    {
+                        "id": "redis-cluster",
+                        "name": "redis-cluster",
+                        "description": "redis-cluster",
+                        "introduce": "redis-cluster"
+                    },
+                    {
+                        "id": "mongo-cluster",
+                        "name": "mongo-cluster",
+                        "description": "mongo-cluster",
+                        "introduce": "mongo-cluster"
+                    },
+                    {
+                        "id": "nginx-upstream",
+                        "name": "nginx-upstream",
+                        "description": "nginx-upstream",
+                        "introduce": "nginx-upstream"
+                    },
+                    {
+                        "id": "redis-cluster",
+                        "name": "redis-cluster",
+                        "description": "redis-cluster",
+                        "introduce": "redis-cluster"
+                    },
+                    {
+                        "id": "apache-hadoop",
+                        "name": "apache-hadoop",
+                        "description": "apache-hadoop",
+                        "introduce": "apache-hadoop"
+                    },
+                    {
+                        "id": "cassandra",
+                        "name": "cassandra",
+                        "description": "cassandra",
+                        "introduce": "In this article, I’m going to examine the science behind making successful UI icons before teaching you how to make your own embeddable icon font. From designing the individual icons to converting them for @font-face embedding, and even licensing them for distribution, we shall be using only free software and online services. How about that? You will not need to rely on any of the esoteric knowledge required to make successful alphanumeric typefaces; just an eye for designing things that may appear very, very small. Ultimately, you should go away with a process for making design elements that extends far beyond the manufacture of simple icons. Before we continue, something should be said about what exactly we are trying to achieve by using icons in our designs in the first place, and what makes one icon more successful than the next. Theory before application. In order to do this, we must consider the icon’s role as part of semiology."
+                    },
+                    {
+                        "id": "ghost-blog",
+                        "name": "ghost-blog",
+                        "description": "ghost-blog",
+                        "introduce": "ghost-blog"
+                    },
+                    {
+                        "id": "mongo-cluster",
+                        "name": "mongo-cluster",
+                        "description": "mongo-cluster",
+                        "introduce": "mongo-cluster"
+                    },
+                    {
+                        "id": "nginx-upstream",
+                        "name": "nginx-upstream",
+                        "description": "nginx-upstream",
+                        "introduce": "nginx-upstream"
+                    },
+                    {
+                        "id": "redis-cluster",
+                        "name": "redis-cluster",
+                        "description": "redis-cluster",
+                        "introduce": "redis-cluster"
+                    },
+                    {
+                        "id": "apache-hadoop",
+                        "name": "apache-hadoop",
+                        "description": "apache-hadoop",
+                        "introduce": "apache-hadoop"
+                    },
+                    {
+                        "id": "cassandra",
+                        "name": "cassandra",
+                        "description": "cassandra",
+                        "introduce": "In this article, I’m going to examine the science behind making successful UI icons before teaching you how to make your own embeddable icon font. From designing the individual icons to converting them for @font-face embedding, and even licensing them for distribution, we shall be using only free software and online services. How about that? You will not need to rely on any of the esoteric knowledge required to make successful alphanumeric typefaces; just an eye for designing things that may appear very, very small. Ultimately, you should go away with a process for making design elements that extends far beyond the manufacture of simple icons. Before we continue, something should be said about what exactly we are trying to achieve by using icons in our designs in the first place, and what makes one icon more successful than the next. Theory before application. In order to do this, we must consider the icon’s role as part of semiology."
+                    },
+                    {
+                        "id": "ghost-blog",
+                        "name": "ghost-blog",
+                        "description": "ghost-blog",
+                        "introduce": "ghost-blog"
+                    },
+                    {
+                        "id": "mongo-cluster",
+                        "name": "mongo-cluster",
+                        "description": "mongo-cluster",
+                        "introduce": "mongo-cluster"
+                    },
+                    {
+                        "id": "nginx-upstream",
+                        "name": "nginx-upstream",
+                        "description": "nginx-upstream",
+                        "introduce": "nginx-upstream"
+                    },
+                    {
+                        "id": "redis-cluster",
+                        "name": "redis-cluster",
+                        "description": "redis-cluster",
+                        "introduce": "redis-cluster"
                     },
                     {
                         "id": "mongo-cluster",
@@ -108,12 +357,15 @@ App.prototype.renderStackList = function() {
 App.prototype.renderStackIntro = function(stackId) {
     var that = this;
     if (stackId === 'index') {
-        $('#stack-intro').html('');
+        $('#main').show();
+        $('#main').removeClass('animation-hidden').addClass('animation-show');
+        $('#intro').html('');
     } else {
         var stackObj = that.storeDataMap[stackId];
         if (stackObj) {
             var htmlStr = that.stackIntroTpl(stackObj);
-            $('#stack-intro').html(htmlStr);
+            $('#intro').removeClass('animation-hidden').addClass('animation-show');
+            $('#intro').html(htmlStr);
         }
     }
 };
